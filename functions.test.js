c3a854087e853c29cc462231f8f29c9e211a4457
gorkhanath sah <gorkhanathsah@gorkhanaths-MacBook-Pro.local>
2020-11-13 08:23:20 -0800

    i comment out expect.assertions then still passed

--- functions.test.js
+++ functions.test.js
@@ -19,7 +19,7 @@ test("admin should be in username",() =>{
 //  working with asyc data or json server
 
 test('User fetched name should be Leanne Graham', () =>{
-    expect.assertions(1);
+    // expect.assertions(1);
     return functions.fetchUser().then( data => {
         expect(data.name).toEqual('Leanne Graham');
     })
