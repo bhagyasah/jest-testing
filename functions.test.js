b778f4f61992d04c4623085c96cd001907c4d0c1
gorkhanath sah <gorkhanathsah@gorkhanaths-MacBook-Pro.local>
2020-11-13 08:17:56 -0800

    working with asyc or json server

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,5 @@
 const functions=require('./functions');
-
+const axios =require('axios');
 // regex ("regular expressions")
 
 
@@ -14,4 +14,13 @@ test("admin should be in username",() =>{
     const userNames=['mangru',"dhepu","loolia"];
 
     expect(userNames).toContain("dhepu");
+})
+
+//  working with asyc data or json server
+
+test('User fetched name should be Leanne Graham', () =>{
+    expect.assertions(1);
+    return functions.fetchUser().then( data => {
+        expect(data.name).toEqual('Leanne Graham');
+    })
 })
