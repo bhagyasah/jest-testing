ca644fde2613b8a6f98f69bb7d9257a21061a0ef
gorkhanath sah <gorkhanathsah@gorkhanaths-MacBook-Pro.local>
2020-11-12 20:55:01 -0800

    i practices with regular expressions and arrays matcher

--- functions.js
+++ functions.js
@@ -2,6 +2,13 @@
 const functions= {
     add: (num1,num2) => num1 + num2 ,
     isNull: () => null,
+    checkValue: (x) => x,
+    createValue: () => {
+        const user={firstName:'ranjit'};
+        user["lastName"]='shah';
+        return user;
+    },
+    
 };
 module.exports=functions;
 
